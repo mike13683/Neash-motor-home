45addf33fe411371d84e2881ed258224b37b2433
mike owens <mike@dragoninfotech.com>
2021-12-08 20:07:44 +0000

    updates

--- assets/js/source.js
+++ assets/js/source.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
 
+    // homepage - things to do
     $('.owl-carousel,.owl-things-to-do').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+    // Our fleet - compare
+    $('.owl-carousel,.owl-compare-rv').owlCarousel({
         loop:true,
         margin:10,
         nav:false,
