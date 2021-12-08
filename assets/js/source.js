d4e40e636340660bc4721c63de3f4ea878dad294
mike owens <mike@dragoninfotech.com>
2021-12-08 15:30:00 +0000

    first commit

---
+++ assets/js/source.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $('.owl-carousel,.owl-things-to-do').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+});
+
+
