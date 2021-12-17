cc3d49311cae24f1241fc6a6ed33241a99f517a4
mike owens <mike@dragoninfotech.com>
2021-12-17 18:42:04 +0000

    updates

--- assets/js/source.js
+++ assets/js/source.js
@@ -1,51 +1,52 @@
 $(document).ready(function(){
 
-    // homepage - things to do
-    $('.owl-carousel,.owl-things-to-do').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            }
-        }
-    })
-
-    // Our fleet - compare
-    $('.owl-carousel,.owl-compare-rv').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            }
-        }
-    })
+    // // homepage - things to do
+    // $('.owl-carousel,.owl-things-to-do').owlCarousel({
+    //     loop:true,
+    //     margin:10,
+    //     nav:false,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         600:{
+    //             items:3
+    //         },
+    //         1000:{
+    //             items:4
+    //         }
+    //     }
+    // })
+    //
+    // // Our fleet - compare
+    // $('.owl-carousel,.owl-compare-rv').owlCarousel({
+    //     loop:true,
+    //     margin:10,
+    //     nav:false,
+    //     responsive:{
+    //         0:{
+    //             items:1
+    //         },
+    //         600:{
+    //             items:3
+    //         },
+    //         1000:{
+    //             items:4
+    //         }
+    //     }
+    // })
 
 
 // cookie
     $(function () {
-        if (localStorage.getItem('popState') != 'shown') {
-            $("#cookie").fadeIn("slow");
+        if(localStorage.getItem('popState') != 'shown'){
+            $("#cookie").delay(4000).fadeIn();
+            localStorage.setItem('popState','shown')
         }
 
-        $('.cookie-btn').click(function (e) {
+        $('.cookie-btn').click(function(e)
+        {
             $('#cookie').fadeOut();
-            localStorage.setItem('popState', 'shown');
         });
     });
 
