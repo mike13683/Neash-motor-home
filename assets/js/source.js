a92147633d3a3e2cf1f0123dfd3d812608cb34d4
mike owens <mike@dragoninfotech.com>
2021-12-21 17:52:47 +0000

    updates

--- assets/js/source.js
+++ assets/js/source.js
@@ -1,55 +1,50 @@
 $(document).ready(function(){
 
-    // // homepage - things to do
-    // $('.owl-carousel,.owl-things-to-do').owlCarousel({
-    //     loop:true,
-    //     margin:10,
-    //     nav:false,
-    //     responsive:{
-    //         0:{
-    //             items:1
-    //         },
-    //         600:{
-    //             items:3
-    //         },
-    //         1000:{
-    //             items:4
-    //         }
-    //     }
-    // })
-    //
-    // // Our fleet - compare
-    // $('.owl-carousel,.owl-compare-rv').owlCarousel({
-    //     loop:true,
-    //     margin:10,
-    //     nav:false,
-    //     responsive:{
-    //         0:{
-    //             items:1
-    //         },
-    //         600:{
-    //             items:3
-    //         },
-    //         1000:{
-    //             items:4
-    //         }
-    //     }
-    // })
+    // homepage - things to do
+    $('.owl-carousel,.owl-things-to-do').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
 
+    // Our fleet - compare
+    $('.owl-carousel,.owl-compare-rv').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
 
 // cookie
-    $(function () {
-        if(localStorage.getItem('popState') != 'shown'){
-            $("#cookie").delay(4000).fadeIn();
-            localStorage.setItem('popState','shown')
+        if(localStorage.getItem('popupState') != 'shown'){
+            $("#cookie").delay(2000).fadeIn();
+            localStorage.setItem('popupState','shown')
         }
 
-        $('.cookie-btn').click(function(e)
-        {
-            $('#cookie').fadeOut();
+        $(".cookie-btn").click(function(){ // You are clicking the close button
+            $('#cookie').fadeOut(); // Now the popup is hidden.
         });
-    });
-
 
 
 });
