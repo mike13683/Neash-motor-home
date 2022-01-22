7465b18048d72637a17d234898c191d57528536e
mike owens <mike@dragoninfotech.com>
2022-01-22 15:23:06 +0000

    updates

--- assets/js/source.js
+++ assets/js/source.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
         loop:true,
         margin:0,
         nav:false,
-        // autoplay:true,
-        // autoplayTimeout:3000,
+        autoplay:true,
+        autoplayTimeout:3000,
         responsive:{
             0:{
                 items:1
@@ -20,14 +20,13 @@ $(document).ready(function(){
         }
     })
 
-
     // Resources
     $('.owl-options').owlCarousel({
         loop:true,
         margin:0,
         nav:false,
-        // autoplay:true,
-        // autoplayTimeout:3000,
+        autoplay:true,
+        autoplayTimeout:3000,
         responsive:{
             0:{
                 items:1
@@ -43,16 +42,107 @@ $(document).ready(function(){
     });
 
 // cookie
-        if(localStorage.getItem('popupState') != 'shown'){
-            $("#cookie").delay(2000).fadeIn();
-            localStorage.setItem('popupState','shown')
-        }
+    if(localStorage.getItem('popupState') != 'shown'){
+        $("#cookie").delay(2000).fadeIn();
+        localStorage.setItem('popupState','shown')
+    }
 
-        $(".cookie-btn").click(function(){ // You are clicking the close button
-            $('#cookie').fadeOut(); // Now the popup is hidden.
-        });
+    $(".cookie-btn").click(function(){ // You are clicking the close button
+        $('#cookie').fadeOut(); // Now the popup is hidden.
+    });
 
+    // GSAP
+    // homepage intro
+    const IntroController = new ScrollMagic.Controller()
+
+    const IntroTween = gsap.timeline()
+    IntroTween.fromTo("#h-intro .intro--bg-image", {autoAlpha:0, x:"-20px"}, {autoAlpha:1, x:0, duration:0.6,})
+    IntroTween.fromTo("#h-intro .intro__content", {autoAlpha:0, y:"-20px"}, {autoAlpha:1, y:0, duration:2.5,})
+
+    const IntroScene = new ScrollMagic.Scene({
+        triggerElement: ".h-intro",
+        reverse:false
+    })
+        .setTween(IntroTween)
+        .addTo(IntroController)
+
+
+    // fade left to right
+    $(".js-leftRight").each(function(){
+
+        const fadeLeftController = new ScrollMagic.Controller()
+
+        const fadeLeftTween = gsap.timeline()
+        fadeLeftTween.fromTo(this, {autoAlpha:0, x:"-20px"}, {autoAlpha:1, x:0, duration:0.6})
+
+        const fadeLeftControllerScene = new ScrollMagic.Scene({
+            triggerElement: this,
+            reverse:false
+        })
+            .setTween(fadeLeftTween)
+            .addTo(fadeLeftController)
+    })
+
+    // fade top to bottom
+    $(".js-top").each(function(){
+        const fadetopController = new ScrollMagic.Controller()
+
+        const fadetopTween = gsap.timeline()
+        fadetopTween.fromTo(this, {autoAlpha:0, y:"-20px"}, {autoAlpha:1, y:0, duration:0.7})
+
+        const fadetopControllerScene = new ScrollMagic.Scene({
+            triggerElement: this,
+            reverse:false
+
+    })
+            .setTween(fadetopTween)
+            .addTo(fadetopController)
+    })
+
+    // fade bottom to top
+    $(".js-bottom").each(function(){
+        const fadebottomController = new ScrollMagic.Controller()
+
+        const fadebottomTween = gsap.timeline()
+        fadebottomTween.fromTo(this, {autoAlpha:0, y:"20px"}, {autoAlpha:1, y:0, duration:0.7})
+
+        const fadebottomControllerScene = new ScrollMagic.Scene({
+            triggerElement: this,
+            reverse:false
+        })
+            .setTween(fadebottomTween)
+            .addTo(fadebottomController)
+    })
+
+    // stagger effect
+    $(".js-stagger img").each(function(){
+        const fadestaggerController = new ScrollMagic.Controller()
+
+        const fadestaggerTween = gsap.timeline()
+        fadestaggerTween.fromTo(this, {autoAlpha:0, x:"10px"}, {autoAlpha:1, x:0, duration:0.5, stagger:1.5})
+
+        const fadestaggerControllerScene = new ScrollMagic.Scene({
+            triggerElement: this,
+            reverse:false
+        })
+            .setTween(fadestaggerTween)
+            .addTo(fadestaggerController)
+    })
+
+    // intro top of most pages
+    $(".js-intro").each(function(){
+        const IntroController = new ScrollMagic.Controller()
+
+        const IntroTween = gsap.timeline()
+        IntroTween.fromTo(".utility-bg-image", {autoAlpha:0, x:"-20px"}, {autoAlpha:1, x:0, duration:0.6,})
+        IntroTween.fromTo(".utility__content", {autoAlpha:0, y:"-20px"}, {autoAlpha:1, y:0, duration:2.3,})
+
+        const IntroScene = new ScrollMagic.Scene({
+            triggerElement: ".intro",
+            reverse:false
+        })
+            .setTween(IntroTween)
+            .addTo(IntroController)
+    })
 
 });
-
-
